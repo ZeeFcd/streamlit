e66927a77355e5ff88b8329902156b932b28ef4d
ZeeFcd <ZeeFcd@protonmail.ch>
2022-11-13 02:02:22 +0100

    little change

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-
+var = st.camera_input("Take a picture")
 if 'picture1' not in st.session_state:
-    st.session_state['picture1'] = st.camera_input("Take a picture")
+    st.session_state['picture1'] = var
 
 if 'picture1' in st.session_state:
     st.image(st.session_state.picture1)
-    st.session_state.picture1 = st.camera_input("Take a picture")
+    st.session_state.picture1 = var
