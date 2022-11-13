25fa64f121283df61a0cb3760430c096b86645a5
ZeeFcd <ZeeFcd@protonmail.ch>
2022-11-13 01:52:35 +0100

    test

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-picture = st.camera_input("Take a picture")
 
-if picture:
-    st.image(picture)
+if 'picture1' not in st.session_state:
+    st.session_state['picture1'] = st.camera_input("Take a picture")
+
+if 'picture1' in st.session_state:
+    st.image(st.session_state.picture1)
+    st.session_state.picture1 = st.camera_input("Take a picture")
