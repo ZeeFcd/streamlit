8ba6c755383fa9e61c52c97ca05a41e0c25e0276
ZeeFcd <ZeeFcd@protonmail.ch>
2022-11-13 02:21:36 +0100

    nalassuk

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import streamlit as st
 
-if 'cam' not in st.session_state:
-    st.session_state.cam = False
+
+def take_picture():
+    st.session_state.picture1 = st.camera_input("Take a picture of your hand")
+
+
+def show_picture():
+    st.image(st.session_state.picture1)
+
 
 if 'picture1' not in st.session_state:
-    st.session_state.picture1 = st.camera_input("Take a picture")
-    st.session_state.cam = True
+    st.session_state.picture1 = None
+
 
 if st.session_state.cam in st.session_state:
     st.image(st.session_state.picture1)
 
+st.button('Take picture', on_click=take_picture)
+st.button('Show picture', on_click=show_picture)
